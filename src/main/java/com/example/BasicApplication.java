8eee58763faa90c43eb4ec28867057db968e2ed4
daniel.borges <daniel.borges@leucotron.com.br>
2019-11-05 12:40:04 -0300

    create DB instantiation method

--- src/main/java/com/example/BasicApplication.java
+++ src/main/java/com/example/BasicApplication.java
@@ -1,12 +1,30 @@
 package com.example;
 
+import com.example.domain.Category;
+import com.example.repositories.CategoryRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicApplication {
+public class BasicApplication implements CommandLineRunner {
 
+    @Autowired
+    private CategoryRepository categoriaRepository;
+    
     public static void main(String[] args) {
         SpringApplication.run(BasicApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        Category cat1  = new Category(null, "Informática");
+        Category cat2  = new Category(null, "Escritório");
+        
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        
     }
 }
