d9ca49e056d910435bb26959c17b440473334e67
daniel.borges <daniel.borges@leucotron.com.br>
2019-11-07 14:28:06 -0300

    correction in Category and Payment constructor

--- src/main/java/com/example/services/CategoryService.java
+++ src/main/java/com/example/services/CategoryService.java
@@ -50,8 +50,9 @@ public class CategoryService {
     }
     
     public Category update(Category obj) {
-        find(obj.getId());
-        return repo.save(obj);
+        Category newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
     }
     
     public void delete(Integer id) {
@@ -67,5 +68,8 @@ public class CategoryService {
         return new Category(objDto.getId(), objDto.getName());
     }
     
+    private void updateData(Category newObj, Category obj) {
+        newObj.setName(obj.getName());
+    }
 
 }
